34d6d350a02f352c052ed7c5fcf94637d59d164c
unknown <sumitbhardwaj106@gmail.com>
2025-04-05 16:10:47 +0530

    initial commit

---
+++ DS/2-Pointers/Easy/removeDuplicates.js
@@ -0,0 +1,26 @@
+
+/** 
+ *  We move two pointers from start - 
+ *  one is moving pointer (which will move fast and keep comparing value with pointer 1). this moving pointer can also be i of for-loop and vice-versa for other loop
+ * 
+*/
+
+// Case if both are same element - Just keep moving fast pointer 
+// case if both are different then 
+// PLACE MOVER POINTER VALUE AHEAD OF FIRST SLOW POINTER + 1
+
+var removeDuplicates = function(nums) {
+    let ptr1 = 0
+    let ptr2 = 1
+
+    while(ptr2 < nums.length){
+        if(nums[ptr1] === nums[ptr2]){
+            ptr2++
+        }else if(nums[ptr1] !== nums[ptr2]){
+            nums[ptr1+1] = nums[ptr2]
+            ptr1++
+            ptr2++
+        }
+    }
+    return new Set(nums).size
+};
