a9981356cd6788fbd1c5d927dd21daf92b1b18f2
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2025-04-07 12:15:04 +0530

    second approach

--- DS/2-Pointers/Easy/removeDuplicates.js
+++ DS/2-Pointers/Easy/removeDuplicates.js
@@ -24,3 +24,18 @@ var removeDuplicates = function(nums) {
     }
     return new Set(nums).size
 };
+
+// second approach - Efficient one
+
+var removeDuplicates = function(nums) {
+    let i = 1;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 1]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;    
+};
