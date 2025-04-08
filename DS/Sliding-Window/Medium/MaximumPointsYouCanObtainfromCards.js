36c1787046de726a20b4a6dda18ce9f229c0056b
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2025-04-08 20:19:35 +0530

    solution

---
+++ DS/Sliding-Window/Medium/MaximumPointsYouCanObtainfromCards.js
@@ -0,0 +1,19 @@
+var maxScore = function(cardPoints, k) {
+    let n = cardPoints.length;
+    let sum = 0;
+
+    // Start with picking first k cards from the start
+    for (let i = 0; i < k; i++) {
+        sum += cardPoints[i];
+    }
+
+    let maxScore = sum;
+
+    // Now shift the window from end
+    for (let i = 1; i <= k; i++) {
+        sum = sum - cardPoints[k - i] + cardPoints[n - i];
+        maxScore = Math.max(maxScore, sum);
+    }
+
+    return maxScore;
+};
